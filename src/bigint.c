0e159d28969c818a62567eeeb81118cbd75d2330
Cyrille Toulet <cyrille.toulet@gmail.com>
2014-10-17 11:48:11 +0200

    Implementation of big int library

---
+++ src/bigint.c
@@ -0,0 +1,74 @@
+#include "bigint.h"
+
+/**
+ * @see bigint.h
+ */
+void
+bgi_init(mbed_bigint *x)
+{
+    mbed_int i;
+
+    for(i = (mbed_int) 0; i <= BIGINT_SIZE; i++)
+        (*x)[i] = 0;
+}
+
+
+/**
+ * @see bigint.h
+ */
+void
+bgi_cpy(mbed_bigint *dest, const mbed_bigint src)
+{
+    mbed_int i;
+
+    for(i = (mbed_int) 0; i <= BIGINT_SIZE; i++)
+        (*dest)[i] = src[i];
+}
+
+
+/**
+ * @see bigint.h
+ */
+void
+bgi_print(const mbed_bigint x)
+{
+    mbed_int i;
+
+    for (i = (mbed_int) 0; i < BIGINT_SIZE - 1; i++)
+        printf("%lx.", (unsigned long) x[i]);
+
+    printf("%lx\n", (unsigned long) x[BIGINT_SIZE]);
+}
+
+
+/**
+ * @see bigint.h
+ */
+mbed_int
+bgi_cmp(const mbed_bigint x, const mbed_bigint y)
+{
+    mbed_int i = (mbed_int) 0;
+
+    while (i <= SIZE) 
+        if (x[i] != y[i++]) 
+            return (mbed_int) (x[i] > y[i] ? 1 : -1);
+
+    return (mbed_int) 0;
+}
+
+
+/**
+ * @see bigint.h
+ */
+mbed_int
+bgi_is_null(const mbed_bigint x)
+{
+    mbed_int i = (mbed_int) 0;
+
+    while (i <= SIZE)
+        if (x[i++] != (mbed_int) 0)
+            return (mbed_int) 1;
+
+    return (mbed_int) 0;
+}
+
--- src/bigint.h
+++ src/bigint.h
@@ -15,4 +15,38 @@
     /* Mbed bigint */
     typedef mbed_int mbed_bigint[SIZE];
 
+    /**
+     * Init to 0 a big int
+     * @param x The big int to init
+     */
+    extern void bgi_init (mbed_bigint *x);
+
+    /**
+     * Copy a big int
+     * @param dest The big int copy destination
+     * @param src The big int to copy
+     */
+    extern void bgi_cpy (mbed_bigint *dest, const mbed_bigint src);
+
+    /**
+     * Print a big int
+     * @param x The big int to print
+     */
+    extern void bgi_print (const mbed_bigint x);
+
+    /**
+     * Compare two big integers
+     * @param x The first big int to compare
+     * @param y The second big int to compare
+     * @return 0 if x=y, -1 if x<y, or 1 if x>y
+     */
+    extern mbed_int bgi_cmp(const mbed_bigint x, const mbed_bigint y);
+
+    /**
+     * Check if a big int is null or not
+     * @param x The big int to check
+     * @return 1 if x is null, or 0 if not
+     */
+    extern mbed_int bgi_is_null(const mbed_bigint x);
+
 #endif
