c1b23ae7bfac9702cce2cb0eb98d9fb2f39de107
Benjamin Burnouf <benjamin.burnouf@hotmail.fr>
2014-10-21 16:39:19 +0200

    Addition test implementation

--- src/test-add.c
+++ src/test-add.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include "bigint.h"
-#include "test-add-dataset.h"
+#include "test-add-dataset.c"
 
 /**
- * test-add-dataset. sera généré par un script python (que j'ai déjà commencé).
- *
- * Il contiendra le tableau suivant :
- *
- *     mbed_int[BIGINT_SIZE][3][100] test_add_dataset;
- * 
- * Reste à faire le main :
- *
- * for (i=0; i < 99; i++)
- * {
- *     mbed_bigint dest;
- *     bgi_init(&dest);
- *     bgi_add(&dest, test_add_dataset[i][0], test_add_dataset[i][1]);
- *     if (bgi_cmp(dest, test_add_dataset[i][2]) != 0)
- *     {
- *         printf("[FAIL] + détails ...\n");
- *         exit(EXIT_FAILURE);
- *     }
- * }
- *
- * printf("[ OK ]\n");
- * exit(EXIT_SUCCESS);
- *
- * Il faut détailler les infos au cas ou le test ne passe pas ;~)
- */
+ test-add-dataset. sera généré par un script python (que j'ai déjà commencé).
+
+ Il contiendra le tableau suivant :
+
+     mbed_int[BIGINT_SIZE][3][100] test_add_dataset;
+*/
+int main() {
+    for (i=0; i < TEST_ADD_COUNT-1; i++)
+    {
+        mbed_bigint dest;
+        bgi_init(&dest);
+        bgi_add(&dest, test_add_dataset[i][0], test_add_dataset[i][1]);
+        if (bgi_cmp(dest, test_add_dataset[i][2]) != 0)
+        {
+            printf("[FAIL]\n\tNombre 1 : ");
+            bgi_print(test_add_dataset[i][0];
+            printf("\n\tNombre 2 : ");
+            bgi_print(test_add_dataset[i][1];
+            printf("\n\tRésultat attendu : ");
+            bgi_print(test_add_dataset[i][2];
+            printf("\n\tRésultat reçu : ");
+            bgi_print(dest);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("[ OK ]\n");
+    exit(EXIT_SUCCESS);
+}
