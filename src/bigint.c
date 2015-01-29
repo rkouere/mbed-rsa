9c93378c40735a8900a12ae4423139b7822ec237
Cyrille TOULET <iwa@centaurus.nerdit.fr>
2015-01-29 12:37:39 +0100

    Add some comment

--- src/bigint.c
+++ src/bigint.c
@@ -16,7 +16,8 @@ bgi_init(mbed_bigint x)
 {
     mbed_int i;
 
-    for(i = 0; i < BIGINT_SIZE + 1; i++)
+    /* Init the 32 digits + the 2 carry's digits to 0 */
+    for(i = 0; i < BIGINT_SIZE + 2; i++)
         x[i] = (mbed_int) 0x0u;
 }
 
@@ -29,7 +30,8 @@ bgi_cpy(mbed_bigint dest, const mbed_bigint src)
 {
     mbed_int i;
 
-    for(i = (mbed_int) 0; i < BIGINT_SIZE + 1 ; i++)
+    /* Copy the 32 digits + the 2 carry's digits to "dest" */
+    for(i = (mbed_int) 0; i < BIGINT_SIZE + 2 ; i++)
         dest[i] = src[i];
 }
 
@@ -43,9 +45,11 @@ bgi_print(const mbed_bigint x)
     #ifdef _COMPUTER_VERSION
         mbed_int i;
 
+        /* Print the the 31 firsts digits of the bigint */
         for (i = (BIGINT_SIZE - 1); i > 0; i--)
             printf("%8x ", x[i]);
 
+	/* And the last one */
         printf("%8x ", x[0]);
     #endif
 }
@@ -57,9 +61,10 @@ bgi_print(const mbed_bigint x)
 mbed_int
 bgi_cmp(const mbed_bigint x, const mbed_bigint y)
 {
-    mbed_int i = BIGINT_SIZE;
+    mbed_int i = BIGINT_SIZE + 1;
 
     while (i > 0) 
+        /* Compare digit by digit */
         if (x[i] != y[i]) 
             return (x[i] > y[i]? 1: -1);
         else
@@ -78,13 +83,18 @@ bgi_add(mbed_bigint dest, const mbed_bigint x, const mbed_bigint y)
     mbed_int carry = 0;
     mbed_int i;
 
-    for (i = (mbed_int) 0; i < BIGINT_SIZE; i++)
+    /* Add two big integers digit by digit */
+    for (i = (mbed_int) 0; i < BIGINT_SIZE + 2; i++)
     {
         dest[i] = (x[i] + y[i] + carry);
-	/* TODO A beautiful test: */
-        carry = (((x[i] + y[i]) < x[i]) || ((x[i] + y[i]) < y[i])) ? (mbed_int) 1: (mbed_int) 0;
+
+        carry = (
+            ((x[i] + y[i]) < x[i]) ||
+            ((x[i] + y[i]) < y[i])
+        ) ? (mbed_int) 1: (mbed_int) 0;
     }
 
+    /* And don't forget the carry! */
     dest[i] += carry;
 }
 
@@ -98,12 +108,14 @@ bgi_sub(mbed_bigint dest, const mbed_bigint x, const mbed_bigint y)
     mbed_int carry = 0;
     mbed_int i;
 
+    /* Subtracts two big integers digit by digit */
     for (i = (mbed_int) 0; i < BIGINT_SIZE; i++)
     {
         dest[i] = (x[i] - y[i] + carry);
         carry = (x[i] + carry) <= y[i]? (mbed_int) -1: (mbed_int) 0;
     }
 
+    /* And don't forget the carry! */
     dest[i] = carry;
 }
 
@@ -112,10 +124,14 @@ bgi_sub(mbed_bigint dest, const mbed_bigint x, const mbed_bigint y)
  * @see bigint.h
  */
 void 
-bgi_mul_int_by_int(mbed_int *dest1, mbed_int *dest2, const mbed_int x, const mbed_int y) 
+bgi_mul_int_by_int(mbed_int *dest1, mbed_int *dest2, const mbed_int x, 
+const mbed_int y)
 {
-    mbed_long_int res = (mbed_long_int) (((mbed_long_int) x) * ((mbed_long_int) y));
+    /* Multiply two integers and store the result in a 64 bits integer */
+    mbed_long_int res = (mbed_long_int) 
+        (((mbed_long_int) x) * ((mbed_long_int) y));
 
+    /* Split the result to extract the carry */
     *dest1 = (mbed_int) (res >> 32);
     *dest2 = (mbed_int) res; 
 }
@@ -125,17 +141,19 @@ bgi_mul_int_by_int(mbed_int *dest1, mbed_int *dest2, const mbed_int x, const mbe
  * @see bigint.h
  */
 void 
-bgi_mul_bigint_by_int(mbed_bigint dest, const mbed_bigint x, const mbed_int y) 
+bgi_mul_bigint_by_int(mbed_bigint dest, const mbed_bigint x, const mbed_int y)
 {
     mbed_int rest;
     mbed_int temp = 0;
     mbed_int carry = 0;
     mbed_int i;
 
-    for (i = (mbed_int) 0; i < BIGINT_SIZE; i++)
+    /* Do a basic multiplication */
+    for (i = (mbed_int) 0; i < BIGINT_SIZE + 1; i++)
     {
         temp = carry;
         bgi_mul_int_by_int(&carry, &rest, x[i], y);
+
         if ( rest > MAX_MBED_INT - 1 - temp) 
         {
             dest[i] = rest + temp;
@@ -144,6 +162,7 @@ bgi_mul_bigint_by_int(mbed_bigint dest, const mbed_bigint x, const mbed_int y)
         else 
             dest[i] = rest + temp;
     }
+
     dest[i] = carry;
 }
 
@@ -156,10 +175,12 @@ bgi_rshift(mbed_bigint x, mbed_int shift)
 {
     mbed_int i;
 
-    for (i = 0; i <= BIGINT_SIZE - shift - 1; i++)
+    /* Shift the digits */
+    for (i = 0; i <= BIGINT_SIZE + 1 - shift - 1; i++)
         x[i] = x[i + shift];
 
-    for (i = BIGINT_SIZE - shift; i <= BIGINT_SIZE; i++)
+    /* Complete digits with "0" */
+    for (i = BIGINT_SIZE + 1 - shift; i <= BIGINT_SIZE + 1; i++)
         x[i] = 0x0u;
 }
 
